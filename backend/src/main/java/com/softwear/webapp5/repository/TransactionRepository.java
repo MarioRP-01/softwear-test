0233c66c48134372d518d4147f4eff49b414f97b
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2022-02-24 18:59:00 +0100

    Created transaction repository and service

---
+++ backend/src/main/java/com/softwear/webapp5/repository/TransactionRepository.java
@@ -0,0 +1,18 @@
+package com.softwear.webapp5.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+import com.softwear.webapp5.model.Coupon;
+import com.softwear.webapp5.model.Transaction;
+import com.softwear.webapp5.model.User;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    public List<Transaction> findByType(String type);
+    public List<Transaction> findByUser(User user);
+    public List<Transaction> findByUsedCoupon(Coupon coupon);
+    public List<Transaction> findByDate(String date);
+
+}
---
+++ backend/src/main/java/com/softwear/webapp5/service/TransactionService.java
@@ -0,0 +1,39 @@
+package com.softwear.webapp5.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.softwear.webapp5.repository.TransactionRepository;
+import com.softwear.webapp5.model.Coupon;
+import com.softwear.webapp5.model.Transaction;
+import com.softwear.webapp5.model.User;
+
+public class TransactionService {
+    
+    private TransactionRepository transactionRepository;
+
+    public Optional<Transaction> findById(Long id) {
+        return transactionRepository.findById(id);
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    public List<Transaction> findByType(String type) {
+        return transactionRepository.findByType(type);
+    }
+
+    public List<Transaction> findByUser(User user) {
+        return transactionRepository.findByUser(user);
+    }
+
+    public List<Transaction> findByUsedCoupon(Coupon coupon) {
+        return transactionRepository.findByUsedCoupon(coupon);
+    }
+
+    public List<Transaction> findByDate(String date) {
+        return transactionRepository.findByDate(date);
+    }
+
+}
