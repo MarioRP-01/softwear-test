c9b19fa620db49a2b9e5965a28bc5dfb47aef28a
pablo.pinillos09@gmail.com <pablo.pinillos09@gmail.com>
2022-03-01 10:43:05 +0100

    mailerClass

---
+++ backend/src/main/java/com/softwear/webapp5/service/MailService.java
@@ -0,0 +1,55 @@
+package com.softwear.webapp5.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class MailService {
+
+    public MailService(){
+
+    }
+
+    public void send(String receiverMail, String subject, String msg) throws Exception{
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", false);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.mailtrap.io");
+        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        }); 
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("from@gmail.com"));
+        message.setRecipients(
+        Message.RecipientType.TO, InternetAddress.parse(receiverMail));
+        message.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+
+    }
+
+
+}
