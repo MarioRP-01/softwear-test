7b55cc67151ce30220cef38ba30cf4d7130f675e
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2022-03-02 19:55:49 +0100

    New Sample Data

--- backend/src/main/java/com/softwear/webapp5/service/DatabaseInitializer.java
+++ backend/src/main/java/com/softwear/webapp5/service/DatabaseInitializer.java
@@ -2,6 +2,8 @@ package com.softwear.webapp5.service;
 
 import javax.annotation.PostConstruct;
 
+import com.softwear.webapp5.model.Product;
+import com.softwear.webapp5.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,34 +12,87 @@ import com.softwear.webapp5.model.Transaction;
 import com.softwear.webapp5.repository.CouponRepository;
 import com.softwear.webapp5.repository.TransactionRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class DatabaseInitializer {
-	
+
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Autowired
 	private CouponRepository couponRepository;
 
 	@Autowired
 	private TransactionRepository transactionRepository;
+
+	@Autowired
+	private CouponService couponService;
 	
 	@PostConstruct
 	public void init() {
 
+		// Products
+		Product leather_coat = new Product("leather coat", "Test Product", new ArrayList<>(), 10);
+		productRepository.save(leather_coat);
+
 		// Coupons
 
+		List<Product> affectedProducts = new ArrayList<>();
+		affectedProducts.add(leather_coat);
+
 		Coupon coupon = new Coupon("ASTONISHOFFER", "total_percentage", "15/02/22", "26/06/22", 0f, 0.5f, null);
+		Coupon coupon2x1 = new Coupon("2X1", "2x1", "13/03/22", "26/06/22", null, null, affectedProducts);
+		Coupon coupon3x2 = new Coupon("TAKEALOOK3X2", "3x2", "12/02/22", "22/02/22", null, null, affectedProducts);
+		Coupon couponLeatherPer = new Coupon("I<3LEATHER", "product_percentage", "15/02/22", "15/06/22", null, 0.4f, affectedProducts);
+		Coupon couponLeatherTot = new Coupon("10LEATHER", "product_amount", "15/02/22", "15/06/22", null, 4.5f, affectedProducts);
 
 		couponRepository.save(new Coupon("10PER", "total_percentage", "15/02/22", "26/06/22", 10.00f, 0.1f, null));
-		couponRepository.save(new Coupon("GIVEME10", "total_fix_amount", "15/02/22", "26/02/22", 10.00f, 2.5f, null));
-		couponRepository.save(new Coupon("2X1", "2x1", "13/03/22", "26/06/22", null, null, null));
-		couponRepository.save(new Coupon("TAKEALOOK3X2", "3x2", "12/02/22", "22/02/22", null, null, null));
+		couponRepository.save(new Coupon("GIVEME10", "total_amount", "15/02/22", "26/02/22", 10.00f, 2.5f, null));
+		couponRepository.save(coupon2x1);
+		couponRepository.save(coupon3x2);
+		couponRepository.save(couponLeatherPer);
+		couponRepository.save(couponLeatherTot);
 		couponRepository.save(coupon);
 
 		// Transactions
 
+		List<Product> productList = new ArrayList<>();
+		for(int i=0; i<3; i++) {
+			productList.add(leather_coat);
+		}
+
 		Transaction transaction = new Transaction("CART", "24/02/22");
 		transaction.setUsedCoupon(coupon);
+		transaction.setProducts(productList);
+		couponService.applyCoupon(transaction);
 		transactionRepository.save(transaction);
-		
+
+		transaction = new Transaction("PROCESSED", "17/02/22");
+		transaction.setUsedCoupon(coupon2x1);
+		transaction.setProducts(productList);
+		couponService.applyCoupon(transaction);
+		transactionRepository.save(transaction);
+
+		transaction = new Transaction("PROCESSED", "17/02/22");
+		transaction.setUsedCoupon(coupon3x2);
+		transaction.setProducts(productList);
+		couponService.applyCoupon(transaction);
+		transactionRepository.save(transaction);
+
+		transaction = new Transaction("PROCESSED", "17/02/22");
+		transaction.setUsedCoupon(couponLeatherPer);
+		transaction.setProducts(productList);
+		couponService.applyCoupon(transaction);
+		transactionRepository.save(transaction);
+
+		transaction = new Transaction("PROCESSED", "17/02/22");
+		transaction.setUsedCoupon(couponLeatherTot);
+		transaction.setProducts(productList);
+		couponService.applyCoupon(transaction);
+		transactionRepository.save(transaction);
+
 	}
 	
 }
