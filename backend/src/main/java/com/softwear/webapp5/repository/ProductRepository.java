b64d19bc61abe2d80bca70d1f8e153761d146c97
Ana Cristina Acosta Hernández <anacristinatecno@gmail.com>
2022-03-03 12:48:15 +0100

    Moved new index and productView to templates folder

--- backend/src/main/java/com/softwear/webapp5/repository/ProductRepository.java
+++ backend/src/main/java/com/softwear/webapp5/repository/ProductRepository.java
@@ -2,13 +2,8 @@ package com.softwear.webapp5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 import com.softwear.webapp5.model.Product;
 
 public interface ProductRepository extends JpaRepository <Product, Long> {
-    public List<Product> findById(String code);
-    public List<Product> findByName(String name);
-    public List<Product> findByType(String type);
-    public List<Product> modify(long id);
+
 }
