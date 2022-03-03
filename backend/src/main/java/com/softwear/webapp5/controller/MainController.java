2a73df1efe6de911879ca7f8973a983fb0119612
Ana Cristina Acosta Hernández <anacristinatecno@gmail.com>
2022-03-03 21:03:53 +0100

    ProductController cleaned and GetMapping index fixed

--- backend/src/main/java/com/softwear/webapp5/controller/MainController.java
+++ backend/src/main/java/com/softwear/webapp5/controller/MainController.java
@@ -1,34 +1,42 @@
 package com.softwear.webapp5.controller;
 
+import com.softwear.webapp5.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	@GetMapping("/productView") //Missing id
-	public String product(Model model) {
-	    return "productView";
-	}
-	
-	@GetMapping("/") //Missing id
-	public String index(Model model) {
-	    return "index";
-	}
-	
-	@GetMapping("/about") //Missing id
-	public String about(Model model) {
-	    return "about";
-	}
-	
-	/*@GetMapping("/userProfile") //Missing id
-	public String user(Model model) {
-	    return "userProfile";
-	}*/
-	
-	@GetMapping("/login") //Missing id
-	public String login(Model model) {
-	    return "login";
-	}
-	
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/productView") //Missing id
+    public String product(Model model) {
+        return "productView";
+    }
+
+    @GetMapping("/") //Missing id
+    public String index(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "index";
+    }
+
+    @GetMapping("/about") //Missing id
+    public String about(Model model) {
+        return "about";
+    }
+
+    @GetMapping("/userProfile") //Missing id
+    public String user(Model model) {
+        return "userProfile";
+    }
+
+    @GetMapping("/login") //Missing id
+    public String login(Model model) {
+        return "login";
+    }
+
 }
