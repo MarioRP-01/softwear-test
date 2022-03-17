a3d66451529d5a6d7ff2f2b5d78045200ea01d7d
JEstebanPerez <j.esteban.2019@alumnos.urjc.es>
2022-03-17 02:00:48 +0100

    Index pageable

--- backend/src/main/java/com/softwear/webapp5/controller/MainController.java
+++ backend/src/main/java/com/softwear/webapp5/controller/MainController.java
@@ -5,6 +5,7 @@ import com.softwear.webapp5.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import org.springframework.stereotype.Controller;
@@ -17,19 +18,23 @@ public class MainController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/") //Missing id
+    @GetMapping("/")
     public String index(Model model, Pageable pageable) {
-        Page<Product> products = productService.findAll(pageable);
+    	Page<Product> products = productService.findAll(PageRequest.of(0, 3));
         model.addAttribute("products", products);
+        model.addAttribute("hasNext", products.hasNext());
+        model.addAttribute("nextPage", products.getNumber()+1);
+        model.addAttribute("prevPage", products.getNumber()-1);
+        model.addAttribute("maxPages", products.getTotalPages());
         return "index";
     }
 
-	@GetMapping("/productView") //Missing id
+	@GetMapping("/productView")
 	public String product(Model model) {
 	    return "productView";
 	}
 	
-	@GetMapping("/about") //Missing id
+	@GetMapping("/about")
 	public String about(Model model) {
 		model.addAttribute("activeMenu", "about");
 	    return "about";
