4ce380ce80f1ae5bc183717d6eabf359d566b992
AnaAcostaH <anacristinatecno@gmail.com>
2022-04-24 23:47:48 +0100

    Product module created

---
+++ frontend/src/app/modules/product/product.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductviewComponent } from './productview/productview.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductviewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductModule { }
