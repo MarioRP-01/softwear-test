35f82366a50ee163a0bde0f4bfc9fc3c41677da6
MarioRP-01 <74364145+MarioRP-01@users.noreply.github.com>
2022-04-28 12:41:25 +0200

    Created and implemented all functions of wishlist view.

--- frontend/src/app/modules/transaction/pages/wishlist/wishlist.component.ts
+++ frontend/src/app/modules/transaction/pages/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '@app/core/api';
+import { TransactionSpecialType } from '@app/shared/data-type';
+import { ProductInTransaction, Transaction } from '@app/shared/model';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +10,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() { }
+  wishlistType = TransactionSpecialType.WISHLIST;
+  cartType = TransactionSpecialType.CART;
+
+  wishlist!: Transaction;
+  products: ProductInTransaction[] = [];
+
+  constructor(
+    private transactionService: TransactionService
+  ) { }
 
   ngOnInit(): void {
+
+    this.refreshWishlist();
   }
 
+  refreshWishlist(): void {
+
+    this.transactionService.getMySpecialTransactionByType(this.wishlistType).subscribe(
+      (result: Transaction) => {
+        this.wishlist = result;
+        this.loadPage();
+
+      }, error => console.log(error)
+    )
+  }
+
+  loadPage() {
+
+    this.wishlist.products.forEach(
+      product => {
+
+        let productInTransaction = {
+          id: product.id,
+          name: product.name,
+          size: product.size,
+          price: product.price,
+          quantity: 1
+        }
+
+        let position = this.productIsPresent(productInTransaction) 
+        if ( position == -1) {
+          this.products.push(productInTransaction)
+
+        } else {
+          this.products[position].quantity++
+
+        }
+      }
+    )
+  }
+
+  private productIsPresent(
+    product: ProductInTransaction): number {
+
+    let isContained = false;
+
+    let productIndex = 0;
+    if (this.products.length !== productIndex) {
+
+      while ((productIndex < this.products.length) && !isContained) {
+
+        isContained = this.products[productIndex].id == product.id;
+        if (!isContained)
+          productIndex++
+      }
+
+    }
+
+    if (!isContained)
+      productIndex = -1
+
+    return productIndex
+  }
+
+
+  addToCart(index: number) {
+
+    this.products[index].quantity = 0
+
+    this.transactionService.addProductByAmountToMyTransaction(this.products[index].id, this.cartType, 1).subscribe(
+      error => console.log(error)
+    )
+
+    this.transactionService.deleteProductFromMyTransaction(this.products[index].id, this.wishlistType).subscribe(
+      error => console.log(error)
+    )
+  }
+
+  deleteFromWishlist(index: number) {
+
+    this.products[index].quantity = 0
+
+    this.transactionService.deleteProductFromMyTransaction(this.products[index].id, this.wishlistType).subscribe(
+      error => console.log(error)
+    )
+  }
+
+  emptyWishlist() {
+
+    this.products = []
+    this.transactionService.deleteAllProductsFromMyTransaction(this.wishlistType).subscribe(
+      error => console.log(error)
+    )
+  }
 }
--- frontend/src/app/modules/transaction/transaction-routing.module.ts
+++ frontend/src/app/modules/transaction/transaction-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WishlistCardComponent } from './components/wishlist-card/wishlist-card.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
 
 const routes: Routes = [
   { path: '', component: CartComponent },
-  { path: 'wishlist', component: WishlistCardComponent}
+  { path: 'wishlist', component: WishlistComponent}
 ];
 
 @NgModule({
