4680eb2e1f8cd8412827f7c4b3beb3658416dff0
MarioRP-01 <marlorosadron@gmail.com>
2022-04-29 10:04:41 +0200

    Fixed bug where you could not enter ProductView without being logged.

--- frontend/src/app/core/resolver/user-logged.resolver.ts
+++ frontend/src/app/core/resolver/user-logged.resolver.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/router';
 import { ShopUser as ShopUserInterface} from '@app/shared/model';
 import { ShopUser } from '@app/shared/classes';
-import { map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { AuthService } from '../authentication';
 
   
@@ -14,19 +14,23 @@ import { AuthService } from '../authentication';
 @Injectable({
   providedIn: 'root'
 })
-export class UserLoggedResolver implements Resolve<ShopUser> {
+export class UserLoggedResolver implements Resolve<ShopUser | null> {
 
   constructor(
     private authService: AuthService
     ){}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ShopUser> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ShopUser | null> {
 
     return this.authService.loadUser().pipe(map(
       response => new ShopUser(response),
       (error: any) => {
         console.log(error)
         return null
-      }));
+      }), catchError(
+        err => {
+          console.log(err)
+          return of(null)          
+        }));
   }
 }
--- frontend/src/app/core/resolver/wishlist.resolver.ts
+++ frontend/src/app/core/resolver/wishlist.resolver.ts
@@ -6,27 +6,27 @@ import {
 } from '@angular/router';
 import { TransactionSpecialType } from '@app/shared/data-type';
 import { Transaction } from '@app/shared/classes';
-import { map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { TransactionService } from '../api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WishlistResolver implements Resolve<Transaction> {
+export class WishlistResolver implements Resolve<Transaction | null> {
 
   constructor(
     private transactionService: TransactionService
   ) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Transaction> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Transaction | null> {
     
     let wishlistType = TransactionSpecialType.WISHLIST;
     return this.transactionService.getMySpecialTransactionByType(wishlistType).pipe(map(
-      response => new Transaction(response),
-      (error: any) => {
-        console.log(error);
-        return null;
-      }
-    ))
+      response => new Transaction(response)
+    ), catchError(
+      err => {
+        console.log(err)
+        return of(null)
+      }))
   }
 }
