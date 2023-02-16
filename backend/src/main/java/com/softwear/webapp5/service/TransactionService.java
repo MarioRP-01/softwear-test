37829a280a15fa452dcbf146e19fb353411c1c2c
MarioRP-01 <marlorosadron@gmail.com>
2023-02-16 17:30:48 +0100

    Remove unused imports

--- backend/src/main/java/com/softwear/webapp5/service/TransactionService.java
+++ backend/src/main/java/com/softwear/webapp5/service/TransactionService.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 
 @Service
